d5ac58865b73dad47ea3da8be52848ff81de3c97
Luis Angel Vanegas <luanvane@bancolombia.com.co>
2020-09-15 21:55:03 -0500

    addition of code

---
+++ src/java/com/udea/ejb/VentasFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.ejb;
+
+import com.udea.entity.Ventas;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Luis Angel
+ */
+@Stateless
+public class VentasFacade extends AbstractFacade<Ventas> implements VentasFacadeLocal {
+
+    @PersistenceContext(unitName = "Lab1.ArqSoft.IntroduccionJEEConcesionarioPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public VentasFacade() {
+        super(Ventas.class);
+    }
+    
+}
